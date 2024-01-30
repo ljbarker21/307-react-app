f677b7c56dff7eda53fbbec8c008d32641eef591
ljbarker <loganjb21@outlook.com>
2024-01-30 15:32:57 -0800

    Frontend can communicate with Backend now

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -56,7 +57,7 @@ const addUser = (user) => {
 };
 
 
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetchUsers()
+            .then((res) => res.json())
+            .then((json) => setCharacters(json["users_list"]))
+            .catch((error) => { console.log(error); });
+      }, [] );
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -25,6 +32,13 @@ function MyApp() {
         <Form handleSubmit={updateList}/>
         </div>
     );
+
+    function fetchUsers() {
+        const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
+
+    
 }
 
 export default MyApp;
